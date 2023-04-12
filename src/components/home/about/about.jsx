b5f4ddb5f55e9ab19db778cf180f3139574e5902
Bappaditya sing <104288057+Bappaditya721135@users.noreply.github.com>
2023-04-12 11:00:53 +0530

    Add slight fix

--- src/components/home/about/about.jsx
+++ src/components/home/about/about.jsx
@@ -3,7 +3,7 @@ import "./about.styles.scss";
 const About = () => {
     return(
         <div className="aboutpage-container">
-            <img src="./img/aboutpage-top-image.jpg" alt="aboutpage-top-image" />
+            <img src="./img/aboutpage-top-image.jpg" alt="aboutpage" />
             <h2>Don't squeeze in sadan when you could relax in a van.</h2>
             <p className="first-paragraph">Our mission is to enliven your road trip with perfect treval  van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch. (Hitch costs extra 😊)</p>
             <p>Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</p>
--- src/components/home/van/van-card/vanCard.jsx
+++ src/components/home/van/van-card/vanCard.jsx
@@ -3,7 +3,7 @@ import "./vanCard.styles.scss";
 const VanCard = ({name, price, category, img}) => {
     return(
             <div className="van-card">
-                <img src={img} alt="van-image" />
+                <img src={img} alt="van" />
                 <div className="van-details">
                     <p className="van-name">{name}</p>
                     <p className="van-price">{price}$/day</p>
