5e48ad694171af4cbdb357a0af75f3b61f021f7e
Bappaditya sing <104288057+Bappaditya721135@users.noreply.github.com>
2023-04-13 16:02:16 +0530

    style vanDetails component

--- src/components/home/van/van-details/vanDetails.jsx
+++ src/components/home/van/van-details/vanDetails.jsx
@@ -1,6 +1,10 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
+
+// style sheet 
+import "./vanDetails.styles.scss";
+
 const VanDetails = () => {
     const {id} = useParams();
     const [van, setVan] = useState({});
@@ -10,12 +14,16 @@ const VanDetails = () => {
         .then((data) => setVan(data))
     }, [])
     return(
-        <div className="van-details">
-            <button>click here to go back</button>
-            <img src={van.img} alt="van" />
-            <h2>{van.name}</h2>
-            <p>{van.price}$/day</p>
-            <p>{van.description}</p>
+        <div className="van-details-page">
+            <button className="go-back-btn">click here to go back</button>
+                <img src={van.img} alt="van" />
+            <div className="van-details">
+                <span className="van-category">{van.category}</span>
+                <h2>{van.name}</h2>
+                <p className="van-details-price"><span>${van.price}</span>/day</p>
+                <p className="van-details-description">{van.description}</p>
+                <button>rent this van</button>
+            </div>
         </div>
     );
 }
